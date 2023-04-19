b1f8fd05d1d4f37f270a0cad2dcc1b52a92c0d28
Granhn <fuenzaladeveloper@gmail.com>
2023-04-18 22:03:16 -0400

    Exports

---
+++ NodeJs/Aprendiendo/module/main.js
@@ -0,0 +1,4 @@
+//const web = require('./myModule');
+const { miNombre } = require('./myModule');
+
+console.log(miNombre);
---
+++ NodeJs/Aprendiendo/module/myModule.js
@@ -0,0 +1,10 @@
+const myWebAddress = true;
+const miNumero = 23;
+const miNombre = "Diego";
+const group = {
+    myWebAddress,
+    miNombre,
+    miNumero
+}
+
+module.exports = group;
